f511dcb7d1157acff8be46c189216ef38a8f6f05
Ajay Singh <helloajaysingh1@gmail.com>
2020-10-03 12:18:20 +0530

    updated exp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import DSC from "../images/dsc-icon.svg";
 import RMGX from "../images/rmgx-icon.png";
+import GCLOUD from "../images/gcloud-icon.svg";
+import GCLOUD2 from "../images/gcloud-icon2.svg";
+// import RMGX from "../images/rmgx-icon.png";
 import "../styles/experience.css";
 const Experience = () => {
   return (
@@ -14,7 +17,7 @@ const Experience = () => {
             <div className="dsc mt-4 justify-content-center text-center">
               <h4 className="text-center">
                 Developer Student Clubs Lead
-                @BIST, Bhopal
+                at BIST, Bhopal
               </h4>
               <a
                 target="_blank"
@@ -51,6 +54,25 @@ const Experience = () => {
               </a>
               <p className="text-muted d-3 text-center pt-4 mb-2">
                 August 2020 - present
+              </p>
+            </div>
+            <hr className="horizontal-line" />
+            <div className="gcloud mt-4 justify-content-center text-center">
+              <h4 className="text-center mb-4">
+                Google Cloud Facilitator at BIST,
+                Bhopal
+              </h4>
+              <a
+                target="_blank"
+                href="https://dsc.community.dev/bansal-institute-of-science-and-technology/ "
+              >
+                <img
+                  className="text-center justify-content-center mt-2"
+                  src={GCLOUD}
+                />
+              </a>
+              <p className="text-muted d-3 text-center">
+                #30DaysOfGoogleCloud | 1st - 31st October 2020
               </p>
             </div>
           </div>
